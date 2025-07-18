1539bb2ebb3eb9810439549ec6c3fd44828cf76e
sahinmeric <sahinmeric22@gmail.com>
2025-07-18 10:01:26 -0500

    moved handleLogin and used useLogin custom hook

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -6,51 +6,19 @@ import {
   Box,
   TextField,
   Button,
-  Snackbar,
-  Alert,
   CircularProgress,
 } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import useSnackbar from "../hooks/useSnackbar";
+import { useLogin } from "../hooks/useLogin";
+
 
 const LoginPage: React.FC = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const {
-    open: snackbarOpen,
-    message: snackbarMessage,
-    severity: snackbarSeverity,
-    showSnackbar,
-    handleClose: handleSnackbarClose,
-  } = useSnackbar();
-  const navigate = useNavigate();
+  const { login, loading } = useLogin();
 
-  const handleLogin = async () => {
-    setLoading(true);
-    try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
-      });
 
-      if (!response.ok) throw new Error("Login failed");
-
-      const data = await response.json();
-      localStorage.setItem("accessToken", data.accessToken);
-      localStorage.setItem("refreshToken", data.refreshToken);
-      showSnackbar("Logged in successfully!", "success");
-
-      setTimeout(() => {
-        navigate("/dashboard");
-      }, 1000);
-    } catch (err) {
-      console.error("Login failed:", err);
-      showSnackbar("Invalid email or password.", "error");
-    } finally {
-      setLoading(false);
-    }
+  const handleSubmit = () => {
+    login(email, password);
   };
 
   return (
@@ -76,21 +44,11 @@ const LoginPage: React.FC = () => {
             onChange={(e) => setPassword(e.target.value)}
             autoComplete="current-password"
           />
-          <Button fullWidth variant="contained" sx={{ mt: 2 }} onClick={handleLogin} disabled={loading}>
-            {loading ? <CircularProgress size={24} color="inherit" /> : "Login"}
+          <Button fullWidth variant="contained" sx={{ mt: 2 }} onClick={handleSubmit} disabled={loading}>
+            {loading ? <CircularProgress size={10} color="inherit" /> : "Login"}
           </Button>
         </Box>
       </Paper>
-      <Snackbar
-        open={snackbarOpen}
-        autoHideDuration={6000}
-        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        onClose={handleSnackbarClose}
-      >
-        <Alert onClose={handleSnackbarClose} severity={snackbarSeverity} sx={{ width: "100%" }}>
-          {snackbarMessage}
-        </Alert>
-      </Snackbar>
     </Container>
   );
 };
