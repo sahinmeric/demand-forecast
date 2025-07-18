8adcd5e191b8b8077b6489b5b70cbda4e5ab07d5
sahinmeric <sahinmeric22@gmail.com>
2025-07-18 09:59:51 -0500

    wrapped with snackbar provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { SnackbarProvider } from "./context/SnackbarContext";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SnackbarProvider>
+        <App />
+      </SnackbarProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
