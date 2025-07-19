582e031e47f737c985babd24932bea640e94844f
sahinmeric <sahinmeric22@gmail.com>
2025-07-18 21:49:41 -0500

    Loader updated with sullHeight support

--- frontend/src/components/Loader.tsx
+++ frontend/src/components/Loader.tsx
@@ -4,6 +4,7 @@ type LoaderProps = {
   message?: string;
   size?: number;
   center?: boolean;
+  fullHeight?: boolean;
   marginBottom?: number;
 };
 
@@ -11,6 +12,7 @@ const Loader = ({
   message = "Loading...",
   size = 40,
   center = true,
+  fullHeight = false,
   marginBottom = 2,
 }: LoaderProps) => {
   return (
@@ -21,6 +23,8 @@ const Loader = ({
         display: "flex",
         flexDirection: "column",
         alignItems: center ? "center" : "flex-start",
+        justifyContent: center ? "center" : "flex-start",
+        minHeight: fullHeight ? "60vh" : undefined,
       }}
     >
       <CircularProgress sx={{ mb: 1 }} size={size} />
