a845f308219055dabb1787faa10bc42fb3c624d5
sahinmeric <sahinmeric22@gmail.com>
2025-07-19 11:32:39 -0500

    renamed ForecastChartPage to ChartPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
-import { Routes, Route, Navigate} from 'react-router-dom';
-import DashboardPage from './pages/DashboardPage';
-import UploadPage from './pages/UploadPage';
-import SalesTablePage from './pages/SalesTablePage';
-import ForecastPage from './pages/ForecastPage';
-import ForecastChartPage from './pages/ForecastChart';
-import HomePage from './pages/HomePage';
-import Navigation from './components/Navigation';
-import PrivateRoute from './components/PrivateRoute';
-import { isAuthenticated } from './auth';
-import RegisterPage from './pages/RegisterPage';
+import { Routes, Route, Navigate } from "react-router-dom";
+import DashboardPage from "./pages/DashboardPage";
+import UploadPage from "./pages/UploadPage";
+import SalesTablePage from "./pages/SalesTablePage";
+import ForecastPage from "./pages/ForecastPage";
+import ChartPage from "./pages/ChartPage";
+import HomePage from "./pages/HomePage";
+import Navigation from "./components/Navigation";
+import PrivateRoute from "./components/PrivateRoute";
+import { isAuthenticated } from "./auth";
+import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
@@ -16,15 +16,59 @@ function App() {
     <>
       <Navigation />
       <Routes>
-        <Route path="/" element={isAuthenticated() ? <Navigate to="/dashboard" replace /> : <HomePage />} />
+        <Route
+          path="/"
+          element={
+            isAuthenticated() ? (
+              <Navigate to="/dashboard" replace />
+            ) : (
+              <HomePage />
+            )
+          }
+        />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
-        <Route path="/upload" element={<PrivateRoute><UploadPage /></PrivateRoute>} />
-        <Route path="/sales" element={<PrivateRoute><SalesTablePage /></PrivateRoute>} />
-        <Route path="/forecast" element={<PrivateRoute><ForecastPage /></PrivateRoute>} />
-        <Route path="/charts" element={<PrivateRoute><ForecastChartPage /></PrivateRoute>} />
-    </Routes>
+        <Route
+          path="/dashboard"
+          element={
+            <PrivateRoute>
+              <DashboardPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/upload"
+          element={
+            <PrivateRoute>
+              <UploadPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/sales"
+          element={
+            <PrivateRoute>
+              <SalesTablePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/forecast"
+          element={
+            <PrivateRoute>
+              <ForecastPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/charts"
+          element={
+            <PrivateRoute>
+              <ChartPage />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
     </>
   );
 }
