277c7a51374261ad1f17c512150f63a831bf7ebc
sahinmeric <sahinmeric22@gmail.com>
2025-07-19 09:07:15 -0500

    auth improved, if token is expired redirect can't reach to private routes

--- frontend/src/auth.ts
+++ frontend/src/auth.ts
@@ -1,21 +1,41 @@
+import { jwtDecode } from "jwt-decode";
+
+type JwtPayload = {
+  exp: number;
+};
+
 export const saveTokens = (accessToken: string, refreshToken: string) => {
-  localStorage.setItem('accessToken', accessToken);
-  localStorage.setItem('refreshToken', refreshToken);
+  localStorage.setItem("accessToken", accessToken);
+  localStorage.setItem("refreshToken", refreshToken);
 };
 
 export const getAccessToken = (): string | null => {
-  return localStorage.getItem('accessToken');
+  return localStorage.getItem("accessToken");
 };
 
 export const getRefreshToken = (): string | null => {
-  return localStorage.getItem('refreshToken');
+  return localStorage.getItem("refreshToken");
 };
 
 export const logout = () => {
-  localStorage.removeItem('accessToken');
-  localStorage.removeItem('refreshToken');
+  localStorage.removeItem("accessToken");
+  localStorage.removeItem("refreshToken");
 };
 
 export const isAuthenticated = (): boolean => {
-  return !!localStorage.getItem('accessToken');
+  const token = getAccessToken();
+  if (!token) return false;
+
+  try {
+    const { exp } = jwtDecode<JwtPayload>(token);
+    if (Date.now() >= exp * 1000) {
+      logout(); // clear expired tokens
+      return false;
+    }
+    return true;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (err) {
+    logout(); // clear invalid tokens
+    return false;
+  }
 };
