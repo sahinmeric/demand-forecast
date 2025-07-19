ef5a43eabef627416ce41673d0b1af38edf899e9
sahinmeric <sahinmeric22@gmail.com>
2025-07-19 10:24:31 -0500

    added route /generate/:sku

--- backend/src/routes/forecast.routes.js
+++ backend/src/routes/forecast.routes.js
@@ -4,6 +4,7 @@ const authMiddleware = require('../middleware/authMiddleware');
 const forecastController = require('../controllers/forecast.controller');
 
 router.post('/generate', authMiddleware, forecastController.generateForecasts);
+router.post('/generate/:sku', authMiddleware, forecastController.generateForecastForSKU);
 router.get('/', authMiddleware, forecastController.getForecasts);
 router.get('/:sku', authMiddleware, forecastController.getForecastsBySKU);
 
