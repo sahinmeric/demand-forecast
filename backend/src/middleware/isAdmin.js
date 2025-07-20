da23a034d2c8702620f042f1221ef0742751839e
sahinmeric <sahinmeric22@gmail.com>
2025-07-20 10:52:42 -0500

    isAdmin added

---
+++ backend/src/middleware/isAdmin.js
@@ -0,0 +1,9 @@
+const isAdmin = (req, res, next) => {
+  if (req.user?.role === 'admin') {
+    return next();
+  }
+  return res.status(403).json({ message: 'Admin access required' });
+};
+
+module.exports = isAdmin;
+
